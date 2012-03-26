b557b98eb27148c81377b8af8b04ca4d06a17ad3
ogom <ogom@hotmail.co.jp>
2012-03-26 18:59:01 +0900

    fix server.js

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World　auto');
+  response.end('Hello World　auto　001');
 }).listen(8080);
