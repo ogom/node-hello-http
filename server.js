d09a35e53bbb74558ea02d3fe25d6fdcbb3227b8
ogom <ogom@hotmail.co.jp>
2012-05-10 14:27:27 +0900

    fix auto

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World:auto-001-002');
+  response.end('Hello World:auto-001-002-003');
 }).listen(8080);
