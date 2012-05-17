acbd390dc13973e2e882b9d1efc7fad7339dd43e
ogom <ogom@hotmail.co.jp>
2012-05-17 16:42:31 +0900

    auto sync test

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World:auto-001-002-003-004');
+  response.end('Hello World');
 }).listen(8080);
