f5281f318535e47d258f68571533521b1080cf29
ogom <ogom@hotmail.co.jp>
2012-05-17 16:48:07 +0900

    auto sync test

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World');
+  response.end('Hello World a');
 }).listen(8080);
