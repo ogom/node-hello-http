7d870ec6dc307f8d5e752c203d3f977604ab5700
ogom <ogom@hotmail.co.jp>
2012-05-30 10:50:13 +0900

    fix abcde

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World abce');
+  response.end('Hello World abcde');
 }).listen(8080);
