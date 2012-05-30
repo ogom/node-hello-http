396de8672d93394911205773adb2342037983182
ogom <ogom@hotmail.co.jp>
2012-05-30 14:29:10 +0900

    fix abcd

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World abc');
+  response.end('Hello World abcd');
 }).listen(80);
