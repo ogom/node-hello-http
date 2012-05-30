c61d1746b1da5829584c5c3edcde2df2e57cd90f
ogom <ogom@hotmail.co.jp>
2012-05-30 11:26:12 +0900

    fix abcdefghijkl

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World abcdefghijk');
+  response.end('Hello World abcdefghijkl');
 }).listen(8080);
