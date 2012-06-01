6f07b7efddd75163c20a14778afe22d3fbdfa120
ogom <ogom@hotmail.co.jp>
2012-06-01 17:37:32 +0900

    fix port

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World abcd');
+  response.end('Hello World abcdf');
 }).listen(8080);
