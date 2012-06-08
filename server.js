2e5fc5b4ab95d3a8f93d0139a46aab03f73538b5
ogom <ogom@hotmail.co.jp>
2012-06-08 14:18:17 +0900

    変更　？？？

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World abc 123 !!!');
+  response.end('Hello World abc 123 !!! ???');
 }).listen(80);
